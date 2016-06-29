c6ab43a3f465f2a3523198874721fff50747aea8
Matt Hoffner <matth@porch.com>
2016-06-29 16:14:56 -0700

    added nodemon and babel stuff

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 'use strict';
 
 import Schema from './back/api/lib/graphql/';
-import { graphql }  from 'graphql';
+import { graphQL }  from 'graphql';
 
-exports.graphqlGET = function graphqlGET (req, res) {
-  (async () => {
+function handleGET(req, res) {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.send('Hello World');
+}
+
+function handlePUT(req, res) {
+  res.writeHead();
+  let result = await (graphQL(Schema, req.query));
+  res.send(result);
+}
+
+export function helloGQL (req, res) {
+  switch(req.method) {
+    case 'GET':
+      handleGET(req, res)
+      break;
+    case 'PUT':
+      handlePUT(req, res)
+      break;
+  }
+}
+
+/*
+(async () => {
     var result = await (graphql(Schema, req.query));
     res.send(result);
   })();
-}
+*/
