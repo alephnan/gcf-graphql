c6b24a2fb810250ac619527b656db5f524c6c149
Matt Hoffner <matth@porch.com>
2016-06-28 17:13:00 -0700

    sorta making sense now

--- back/api/lib/db.js
+++ back/api/lib/db.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const Promise = require('bluebird');
+const Elasticsearch = require('elasticsearch');
 
 const config = {
 
 };
 
-const client = new ElasticDB.Client(config);
+const client = new Elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
 
 module.exports = (method, params) => {
   return Promise.fromCallback(cb => client[method](params, cb));
